a006731e29e5b0ce088a0b7d5f2b5ed217bcf05d
Eugemo <mariaeugeniamoreno@hotmail.com>
2021-04-17 14:28:45 -0300

    agrego funcionalidad a buscar

--- js/main.js
+++ js/main.js
@@ -40,41 +40,79 @@ function mOut(obj) {
 
 
 //formulario buscar en index
-//
+//Opcion 1 - envia a otra pag con los productos elegidos
 
-var gabinetes = document.querySelectorAll('[id=gabinetes]');
-var monitores = document.querySelectorAll('[id=monitores]');
-var combos = document.querySelectorAll('[id=combos]');
-var productos = document.querySelectorAll('[id=productos]');
-
-
-function filtrado() {
-    console.log('entro al script');
-    const select = document.getElementById('select');
-    console.log(select.value + ' leo seleccion');
+/*function filtrado() {
+    //console.log('entro al script');
+    let select = document.getElementById('select');
+   // console.log(select.value + ' leo seleccion');
     //console.log(gabinetes);
    
 
-    switch (select.value) {
-        case 1:
-           // console.log('entro por monitores');
-           // productos.childNodes;
-            console.log(productos);
-            //let id = "monitores";
-            //console.log(id);
-            break;
+    if (select.value === '1') {
+        goTo('./producto.html')
+    }else if (select.value === '2') {
+        goTo('./producto.html')
+    }else if(select.value === '3') {
+            goTo('./producto.html')    
 
-        case 2:
-            console.log('entro por gabinetes');
-            // let id = "monitores";
-            //console.log(id);
-            break;
+    }
+    
+}
 
-        case 3:
-            console.log('entro por combos');
-            //let id = "monitores";
-            //console.log(id);
-            break;
+function goTo(link){
+    let a = document.createElement('a');
+    a.href = link;
+    a.click();
+    a.remove();
+}*/
 
+//formulario buscar en index
+//Opcion 2
+
+const gabinetes = document.querySelectorAll("[id=gabinetes]");
+const monitores = document.querySelectorAll("[id=monitores]");
+const combos = document.querySelectorAll("[id=combos]");
+function filtrado() {
+	let select = document.getElementById("select");
+	let productos = document.getElementById("productos");
+	if (select.value === "1") {
+		productos.innerHTML = "";
+		let row = document.createElement("div");
+		row.className = "row row-cols-1 row-cols-md-3 g-4";
+		let auxDiv = productos.appendChild(row);
+		for (let i = 0; i < monitores.length; i++) {
+			let col = document.createElement("div");
+			col.className = "col";
+			col.appendChild(monitores[i]);
+			auxDiv.appendChild(col);
+		}
+	} else if (select.value === "2") {
+		productos.innerHTML = "";
+		let row = document.createElement("div");
+		row.className = "row row-cols-1 row-cols-md-3 g-4";
+		let auxDiv = productos.appendChild(row);
+		for (let i = 0; i < gabinetes.length; i++) {
+			let col = document.createElement("div");
+			col.className = "col";
+			col.appendChild(gabinetes[i]);
+			auxDiv.appendChild(col);
+		}
+	} else if (select.value === "3") {
+		productos.innerHTML = "";
+		let row = document.createElement("div");
+		row.className = "row row-cols-1 row-cols-md-3 g-4";
+		let auxDiv = productos.appendChild(row);
+		for (let i = 0; i < combos.length; i++) {
+			let col = document.createElement("div");
+			col.className = "col";
+			col.appendChild(combos[i]);
+			auxDiv.appendChild(col);
+		}        
+	} else{
+        let a = document.createElement('a');
+        a.href = './index.html';
+        a.click();
+        a.remove();
     }
 }
