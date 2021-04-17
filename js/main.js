4e8e5ab4f0e4aa22ef0e61892c5dd53dbd86447c
Eugemo <mariaeugeniamoreno@hotmail.com>
2021-04-17 09:56:58 -0300

    intento funcionalidad buscar

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 
 document.getElementById("button-up").addEventListener("click", scrollUp);
 
-function scrollUp(){
+function scrollUp() {
 
     var currentScroll = document.documentElement.scrollTop || document.body.scrollTo;
 
-    if (currentScroll > 0){
-        window.scrollTo (0,0);
+    if (currentScroll > 0) {
+        window.scrollTo(0, 0);
     }
 }
 
@@ -15,13 +15,13 @@ function scrollUp(){
 
 buttonUp = document.getElementById("button-up");
 
-window.onscroll = function(){
+window.onscroll = function () {
 
     var scroll = document.documentElement.scrollTop;
 
-    if (scroll > 500){
+    if (scroll > 500) {
         buttonUp.style.transform = "scale(1)";
-    }else if(scroll < 500){
+    } else if (scroll < 500) {
         buttonUp.style.transform = "scale(0)";
     }
 
@@ -30,34 +30,51 @@ window.onscroll = function(){
 //animacion boton ver mas
 function mOver(obj) {
     obj.innerHTML = "VER MAS"
-  }
-  
-  function mOut(obj) {
+}
+
+function mOut(obj) {
     obj.innerHTML = "Ver Mas"
-  }
+}
+
+
+
 
 //formulario buscar en index
 //
 
-function filtrado(){
+var gabinetes = document.querySelectorAll('[id=gabinetes]');
+var monitores = document.querySelectorAll('[id=monitores]');
+var combos = document.querySelectorAll('[id=combos]');
+var productos = document.querySelectorAll('[id=productos]');
+
+
+function filtrado() {
     console.log('entro al script');
     const select = document.getElementById('select');
     console.log(select.value + ' leo seleccion');
-    
-    if(select.value == "1"){
-        console.log('entro por monitores')
-        var id = "monitores";
-        console.log(id);
-        //document.getElementById(this) != id.value.hidde();
-        document.getElementById("monitores").show();             
-         
-    }else{
-        if(select.value == "2"){
-            console.log('entro por gabinetes')
+    //console.log(gabinetes);
+   
 
-        }else{
-            console.log('entro por combos')   
-        //alert.style.visibility = 'hidden';
-        }
-    }    
+    switch (select.value) {
+        case 1:
+           // console.log('entro por monitores');
+           // productos.childNodes;
+            console.log(productos);
+            //let id = "monitores";
+            //console.log(id);
+            break;
+
+        case 2:
+            console.log('entro por gabinetes');
+            // let id = "monitores";
+            //console.log(id);
+            break;
+
+        case 3:
+            console.log('entro por combos');
+            //let id = "monitores";
+            //console.log(id);
+            break;
+
+    }
 }
